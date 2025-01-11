1638ae5f8a0a8fb67e82b44fe7aa24db48eb166e
bargiro1 <76563bar@gmail.com>
2025-01-11 18:04:44 +0200

    Add posts route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_CONNECT);
 const db = mongoose.connection;
 db.on("error", (error) => console.error("Database connection error:", error));
 db.once("open", () => console.log("Connected to database"));
@@ -14,6 +14,8 @@ db.once("open", () => console.log("Connected to database"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const postsRoute = require("./Routes/posts_route");
+app.use("/posts", postsRoute);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);

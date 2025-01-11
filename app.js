049243b7eb802f8baf4189e3f43c6336fdb7b6fe
bargiro1 <76563bar@gmail.com>
2025-01-11 16:28:20 +0200

    Commect to db

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error("Database connection error:", error));
+db.once("open", () => console.log("Connected to database"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
